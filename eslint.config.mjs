7648ad994c11ddd275887827ec26a9f902074037
jesse <jonesvandevelde@gmail.com>
2025-09-10 15:50:12 +0200

    eslint installation done

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,79 @@
-// @ts-check
 import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import angular from 'angular-eslint';
 import stylistic from '@stylistic/eslint-plugin';
 
-export default tseslint.config(
+const eslintConfig = defineConfig(
+  eslint.configs.recommended,
+  stylistic.configs.recommended,
   {
-    files: ["**/*.ts"],
+    rules: {
+      '@stylistic/comma-dangle': [
+        'error',
+        'always-multiline',
+      ],
+      '@stylistic/padding-line-between-statements': ['error',
+        { blankLine: 'always', prev: '*', next: 'return' },
+        { blankLine: 'always', prev: '*', next: ['const', 'let'] },
+        { blankLine: 'always', prev: ['const', 'let'], next: '*' },
+        { blankLine: 'any', prev: ['const', 'let'], next: ['const', 'let'] },
+        { blankLine: 'always', prev: '*', next: 'multiline-const' },
+        { blankLine: 'always', prev: 'multiline-const', next: '*' },
+        { blankLine: 'always', prev: '*', next: 'multiline-let' },
+        { blankLine: 'always', prev: 'multiline-let', next: '*' },
+        { blankLine: 'always', prev: '*', next: 'multiline-expression' },
+        { blankLine: 'always', prev: 'multiline-expression', next: '*' },
+        { blankLine: 'always', prev: '*', next: 'block-like' },
+        { blankLine: 'always', prev: 'block-like', next: '*' },
+        { blankLine: 'any', prev: 'case', next: 'case' },
+        { blankLine: 'any', prev: 'case', next: 'default' },
+        { blankLine: 'always', prev: ['import'], next: '*' },
+        { blankLine: 'any', prev: ['import'], next: ['import'] },
+        { blankLine: 'always', prev: ['expression'], next: '*' },
+        { blankLine: 'always', prev: '*', next: ['expression'] },
+        { blankLine: 'any', prev: ['expression'], next: ['expression'] },
+        { blankLine: 'any', prev: '*', next: 'break' },
+      ],
+      '@stylistic/semi': ['error', 'always'],
+    },
+  },
+);
+
+export default defineConfig(
+  {
+    ignores: ['node_modules', 'dist'],
+  },
+  {
+    files: ['**/*.mjs', '**/*.js'],
+    extends: eslintConfig,
+  },
+  {
+    files: ['**/*.ts'],
     plugins: {
-      '@stylistic': stylistic
+      '@stylistic': stylistic,
     },
     extends: [
-      eslint.configs.recommended,
+      eslintConfig,
       ...tseslint.configs.all,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
     ],
     processor: angular.processInlineTemplates,
     rules: {
-      '@stylistic/comma-dangle': ["error", "always"],
-      "@angular-eslint/directive-selector": [
-        "error",
+      '@angular-eslint/directive-selector': [
+        'error',
         {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
+          type: 'attribute',
+          prefix: 'btj',
+          style: 'camelCase',
         },
       ],
-      "@angular-eslint/component-selector": [
-        "error",
+      '@angular-eslint/component-selector': [
+        'error',
         {
-          type: "element",
-          prefix: "app",
+          type: 'element',
+          prefix: 'btj',
         },
       ],
     },
@@ -39,27 +81,28 @@ export default tseslint.config(
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
-      }
-    }
+      },
+    },
   },
   {
-    files: ["**/*.html"],
-    extends: [
-      ...angular.configs.templateAll,
-    ],
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateAll],
     rules: {
-      "@angular-eslint/template/i18n": 'off',
-      '@angular-eslint/template/attributes-order': ['error', {
-        alphabetical: true,
-        order: [
-          'STRUCTURAL_DIRECTIVE',
-          'ATTRIBUTE_BINDING',
-          'TWO_WAY_BINDING',
-          'OUTPUT_BINDING',
-          'INPUT_BINDING',
-          'TEMPLATE_REFERENCE',
-        ],
-      }],
+      '@angular-eslint/template/i18n': 'off',
+      '@angular-eslint/template/attributes-order': [
+        'error',
+        {
+          alphabetical: true,
+          order: [
+            'STRUCTURAL_DIRECTIVE',
+            'ATTRIBUTE_BINDING',
+            'TWO_WAY_BINDING',
+            'OUTPUT_BINDING',
+            'INPUT_BINDING',
+            'TEMPLATE_REFERENCE',
+          ],
+        },
+      ],
     },
-  }
+  },
 );
