90483287f20293fb3bbd371571948e25d3c7ba83
jesse <jonesvandevelde@gmail.com>
2025-09-11 11:09:47 +0200

    resolve merge conflict

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import './fontawesome'
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
