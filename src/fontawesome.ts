8e22814beaf723e0ee218790c4a608af4a4ebfc3
jesse <jonesvandevelde@gmail.com>
2025-09-11 15:34:19 +0200

    crossing the ts

--- src/fontawesome.ts
+++
@@ -1,4 +0,0 @@
-import { library, icon } from '@fortawesome/fontawesome-svg-core'
-import { faThumbsUp } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faThumbsUp)
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import './fontawesome'
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
