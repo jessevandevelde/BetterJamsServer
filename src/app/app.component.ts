a4cd1e724e6507be036213a624ebfb8d27d38f36
jesse <jonesvandevelde@gmail.com>
2025-09-15 13:49:19 +0200

    add roompage and reorganise room component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,11 @@
 import { Component } from '@angular/core';
-import { QueueRowComponent } from './room-page/components/queue-row/queue-row.component';
 import { RouterOutlet } from '@angular/router';
-import dummyData from './dummy-data/track-data.json';
-import { Track } from './types/track.interfaces';
-import { MediaPlayerComponent } from './room-page/components/media-player/media-player.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [RouterOutlet, MediaPlayerComponent, QueueRowComponent],
+    imports: [RouterOutlet],
 })
-export class AppComponent {
-  title = 'SpotifyBetterJams';
-  dummyData = dummyData;
-  trackData: Track;
-  upvoteCount = 0;
-  upvoted = false;
+export class AppComponent { }
 
-  constructor() {
-    this.trackData = this.createTrackData(dummyData);
-  }
-
-  private createTrackData(data: typeof dummyData): Track {
-    return {
-      albumCoverUrl: data.album.images[0].url,
-      songName: data.name,
-      artistName: data.artists[0].name,
-      songDuration: data.duration_ms,
-    }
-  }
-
-  protected vote() {
-    this.upvoted = true;
-    this.upvoteCount++;
-    console.log('upvoted')
-  }
-
-  protected removeVote() {
-    this.upvoted = false;
-    this.upvoteCount--;
-  }
- }
--- src/app/room-page/room-page.component.ts
+++ src/app/room-page/room-page.component.ts
@@ -1,11 +1,44 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { QueueRowComponent } from './components/queue-row/queue-row.component';
+import { Track } from '../types/track.interfaces';
+import trackData from '../dummy-data/track-data.json';
+import { MediaPlayerComponent } from "./components/media-player/media-player.component";
 
 @Component({
   selector: 'app-room-page',
   standalone: true,
-  imports: [],
-  template: `<p>room-page works!</p>`,
+  imports: [QueueRowComponent, MediaPlayerComponent],
+  templateUrl: './room-page.component.html',
   styleUrl: './room-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RoomPageComponent { }
+export class RoomPageComponent { 
+    dummyData = trackData;
+  trackData: Track;
+  upvoteCount = 0;
+  upvoted = false;
+
+  constructor() {
+    this.trackData = this.createTrackData(trackData);
+  }
+
+  private createTrackData(data: typeof trackData): Track {
+    return {
+      albumCoverUrl: data.album.images[0].url,
+      songName: data.name,
+      artistName: data.artists[0].name,
+      songDuration: data.duration_ms,
+    }
+  }
+
+  protected vote() {
+    this.upvoted = true;
+    this.upvoteCount++;
+    console.log('upvoted')
+  }
+
+  protected removeVote() {
+    this.upvoted = false;
+    this.upvoteCount--;
+  }
+}
