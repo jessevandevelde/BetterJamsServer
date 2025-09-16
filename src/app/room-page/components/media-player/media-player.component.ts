e764a96524352714c7191533371603b2c4245894
jesse <jonesvandevelde@gmail.com>
2025-09-16 10:00:53 +0200

    media player component layout and styles done

--- src/app/room-page/components/media-player/media-player.component.ts
+++ src/app/room-page/components/media-player/media-player.component.ts
@@ -1,13 +1,17 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Track } from '../../../types/track.interfaces';
+import { faPause } from '@fortawesome/free-solid-svg-icons';
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
+
 
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
   styleUrl: './media-player.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [FaIconComponent],
 })
 export class MediaPlayerComponent { 
+  pause = faPause
   track = input.required<Track>();
-  
 }
