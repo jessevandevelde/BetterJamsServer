93ea71ba612140b61ad262ea8e065e1ffce69c30
jesse <jonesvandevelde@gmail.com>
2025-09-16 11:04:40 +0200

    progress bar animation and percentage calculation

--- src/app/room-page/components/media-player/media-player.component.ts
+++ src/app/room-page/components/media-player/media-player.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, effect, inject, input } from '@angular/core';
 import { Track } from '../../../types/track.interfaces';
 import { faPause } from '@fortawesome/free-solid-svg-icons';
 import { FaIconComponent } from "@fortawesome/angular-fontawesome";
@@ -14,4 +14,35 @@ import { FaIconComponent } from "@fortawesome/angular-fontawesome";
 export class MediaPlayerComponent { 
   pause = faPause
   track = input.required<Track>();
+  progress = 0;
+  private cd: ChangeDetectorRef;
+  
+  progressPercentage: number = 0;
+  
+  constructor() {
+    this.cd = inject(ChangeDetectorRef);
+
+    effect(() => {
+      const track = this.track();
+      const progress = this.progress // Needs to be replaced by signal input
+
+      this.progressPercentage = this.getProgressPercentage(track.songDuration, progress);
+    })
+
+    setInterval(() => {
+      const progress = this.progress >= this.track().songDuration 
+        ? 0 
+        : this.progress + 1000;
+      
+      this.progress = progress;
+
+      this.progressPercentage = this.getProgressPercentage(this.track().songDuration, progress)
+      this.cd.detectChanges();
+    }, 1000)
+  }
+
+  private getProgressPercentage(songDuration: number, progress: number): number {
+    this.progressPercentage = (progress / songDuration) * 100;
+    return this.progressPercentage;
+  }
 }
