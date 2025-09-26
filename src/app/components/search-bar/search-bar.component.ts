2edbd8bb66c23eef0419434b90ed5ee3ff62877f
jesse <jonesvandevelde@gmail.com>
2025-09-26 10:52:46 +0200

    search bar frontend done with working clear
    no responsive styles yet

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,16 +1,22 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { SearchBarService } from './search-bar.service';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faMagnifyingGlass, faX } from '@fortawesome/free-solid-svg-icons';
+
+;
 
 @Component({
   selector: 'btj-search-bar',
-  imports: [],
+  imports: [FaIconComponent],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchBarComponent {
-  private readonly searchBarService: SearchBarService;
+  protected magnifyingGlass = faMagnifyingGlass;
+  protected closeIcon = faX;
 
+  private readonly searchBarService: SearchBarService;
   public constructor() {
     this.searchBarService = inject(SearchBarService);
   }
@@ -19,5 +25,13 @@ export class SearchBarComponent {
     const q = 'aint hard';
 
     this.searchBarService.search(q).subscribe();
+  }
+
+  protected clearInput(): void {
+    const searchField = document.querySelector<HTMLInputElement>('.search-input');
+
+    if (searchField !== null) {
+      searchField.value = '';
+    }
   }
 }
