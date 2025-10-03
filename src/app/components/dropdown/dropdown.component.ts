c548b5abffae5acd46e91e0949e9912259c807ce
jesse <jonesvandevelde@gmail.com>
2025-10-03 15:03:22 +0200

    dropdown track component styling done

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -1,8 +1,9 @@
+import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'btj-search-dropdown',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: `../dropdown/dropdown.component.html`,
   styleUrl: '../dropdown/dropdown.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
