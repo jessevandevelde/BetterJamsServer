cadb5617494a9600e3cfb5eaeddcb03c5961c8fe
jesse <jonesvandevelde@gmail.com>
2025-10-06 15:26:46 +0200

    use ngsrc instead of src

--- src/app/components/search-bar/components/search-result/search-result.component.ts
+++ src/app/components/search-bar/components/search-result/search-result.component.ts
@@ -1,9 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ButtonComponent } from 'src/app/components/button/button.component';
 
 @Component({
   selector: 'btj-search-result',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, NgOptimizedImage],
   templateUrl: './search-result.component.html',
   styleUrl: './search-result.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
