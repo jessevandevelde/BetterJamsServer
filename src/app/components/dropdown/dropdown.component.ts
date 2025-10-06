b472d2d464c830f237984890e770931a651acec7
jesse <jonesvandevelde@gmail.com>
2025-10-06 09:41:57 +0200

    push voor hulp

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -1,9 +1,8 @@
-import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'btj-search-dropdown',
-  imports: [NgOptimizedImage],
+  imports: [],
   templateUrl: `../dropdown/dropdown.component.html`,
   styleUrl: '../dropdown/dropdown.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
