b4da28c94f88e27711326265d81516cb779fdc4c
jesse <jonesvandevelde@gmail.com>
2025-10-13 08:50:06 +0200

    search bar dropdown opens at the right time

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, effect, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faMagnifyingGlass, faX } from '@fortawesome/free-solid-svg-icons';
 import { SearchDropdownComponent } from '../dropdown/dropdown.component';
@@ -20,23 +20,16 @@ export class SearchBarComponent {
   public isLoading = input<boolean>();
   protected searchValueChange = output<string>();
   protected clearSearch = output();
-
   protected magnifyingGlass = faMagnifyingGlass;
   protected closeIcon = faX;
   protected showDropdown = false;
-
-  public constructor() {
-    this.initializeTracksEffect();
-  }
+  private isFocused = false;
 
   protected search(event: Event): void {
     /* eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion */
     const target = event.target as HTMLInputElement;
 
     this.searchValueChange.emit(target.value);
-
-    /* eslint-disable-next-line no-console */
-    console.log(target.value);
   }
 
   protected clearInput(): void {
@@ -44,24 +37,12 @@ export class SearchBarComponent {
   }
 
   protected openDropdown(): void {
-    if (!this.value()) {
-      return;
-    }
-
+    this.isFocused = true;
     this.showDropdown = true;
   }
 
   protected closeDropdown(): void {
+    this.isFocused = false;
     this.showDropdown = false;
-  }
-
-  private initializeTracksEffect(): void {
-    effect(() => {
-      if (!this.showDropdown) {
-        return;
-      }
-
-      this.tracks()?.length ? this.openDropdown() : this.closeDropdown();
-    });
   }
 }
