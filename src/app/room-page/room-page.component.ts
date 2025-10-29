6cdbef396d0c4f1ead2aaee3bbd8d702e4e5ef27
Jesse van de Velde <jonesvandevelde@gmail.com>
2025-10-29 13:33:09 +0100

    show playing track in mediaplayer

--- src/app/room-page/room-page.component.ts
+++ src/app/room-page/room-page.component.ts
@@ -2,13 +2,11 @@ import type { Signal } from '@angular/core';
 import { ChangeDetectionStrategy, Component, ChangeDetectorRef, inject } from '@angular/core';
 import { QueueRowComponent } from './components/queue-row/queue-row.component';
 import type { Track } from '../types/track.interfaces';
-import trackData from '../dummy-data/track-data.json';
 import { MediaPlayerComponent } from './components/media-player/media-player.component';
 import { SearchBarComponent } from '../components/search-bar/search-bar.component';
 import { Store } from '@ngrx/store';
 import { RoomPageActions } from './store';
-import { selectIsLoading, selectQuery, selectQueueTracks } from './store/room-page.selectors';
-import { selectTracks } from './store/room-page.selectors';
+import { selectIsLoading, selectQuery, selectQueueTracks, selectTracks } from './store/room-page.selectors';
 import { RoomPageService } from './room-page.service';
 import { getQueueTracks } from './store/room-page.actions';
 import { WebsocketService } from '../services/websocket.service';
@@ -28,33 +26,29 @@ export class RoomPageComponent {
   public upvoted = false;
   public isPlaying = true;
   public isLoading: Signal<boolean>;
-  protected dummyData = trackData;
-  protected trackData: Track;
+  protected trackData: Track | null = null;
   protected progress = 0;
   protected progressPercentage = 0;
   protected searchValue: Signal<string>;
   protected searchResult: Signal<Track[]>;
   protected queueTracks: Signal<Track[]>;
   private readonly cd: ChangeDetectorRef = inject(ChangeDetectorRef);
-  private readonly store: Store;
+  private readonly store = inject<Store>(Store);
   private readonly roomPageService = inject(RoomPageService);
-  private readonly websocketService: WebsocketService;
+  private readonly websocketService: WebsocketService = inject(WebsocketService);
 
   public constructor() {
-    /* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */
-    this.store = inject(Store);
-    this.websocketService = inject(WebsocketService);
     this.searchValue = this.store.selectSignal(selectQuery);
     this.searchResult = this.store.selectSignal(selectTracks);
     this.isLoading = this.store.selectSignal(selectIsLoading);
     this.queueTracks = this.store.selectSignal(selectQueueTracks);
-    this.trackData = this.createTrackData(trackData);
+
     this.initializeQueueUpdatedWebsocket();
     this.getCurrentTrack();
     this.store.dispatch(getQueueTracks());
 
     setInterval(() => {
-      if (this.isPlaying) {
+      if (this.isPlaying && this.trackData) {
         const progress = this.progress >= this.trackData.durationMs
           ? 0
           : this.progress + ONE_SECOND_IN_MS;
@@ -67,8 +61,7 @@ export class RoomPageComponent {
 
   protected initializeQueueUpdatedWebsocket(): void {
     this.websocketService.socket.on('queue-updated', () => {
-      this.store.dispatch(getQueueTracks(),
-      );
+      this.store.dispatch(getQueueTracks());
     });
   }
 
@@ -103,22 +96,11 @@ export class RoomPageComponent {
   }
 
   protected getCurrentTrack(): void {
-    this.websocketService.socket.on('current-track', (track: Track) => {
-      console.log('Ontvangen huidige track:', track);
-      this.trackData = track;
-      this.progress = 0;
+    this.websocketService.socket.on('current-track', (data: { track: Track, progressMs: number }) => {
+      console.log('Received current track:', data);
+      this.trackData = data.track;
+      this.progress = data.progressMs;
       this.cd.detectChanges();
     });
-  }
-
-  private createTrackData(data: typeof trackData): Track {
-    return {
-      albumCoverUrl: data.album.images[0].url,
-      name: data.name,
-      artists: data.artists[0].name,
-      durationMs: data.duration_ms,
-      id: '',
-      uri: '',
-    };
   }
 }
