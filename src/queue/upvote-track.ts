b3e983bbaf39881336bf450c7d7f2b1dbe40186c
Jesse van de Velde <jonesvandevelde@gmail.com>
2025-11-14 10:46:19 +0100

    upvote error handeling

--- src/queue/upvote-track.ts
+++ src/queue/upvote-track.ts
@@ -1,19 +1,25 @@
 import type { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import { getQueue } from './queue';
+import { handleApiError, HttpErrorCause } from '../helpers/errors.helpers';
 
 export function upvoteTrack(req: Request<null, null, { userId: string, trackUuid: string }>, res: Response): void {
   try {
     const queue = getQueue();
 
-    if (!queue.queue.length || !queue.getTrack(req.body.trackUuid)) {
-      throw new Error();
+    const track = queue.getTrack(req.body.trackUuid);
+
+    if (!queue.queue.length || !track) {
+      throw new Error('track not found', {
+        cause: new HttpErrorCause(StatusCodes.BAD_REQUEST),
+      });
     }
 
     queue.upvoteTrack(req.body.userId, req.body.trackUuid);
 
-    res.status(204);
+    res.send(StatusCodes.NO_CONTENT);
   }
-  catch (_error) {
-    res.status(500);
+  catch (error) {
+    handleApiError(error, res);
   }
 }
