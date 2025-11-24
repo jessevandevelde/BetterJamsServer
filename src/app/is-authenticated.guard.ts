026d9a558822941d3524633799c8299b9edf9aac
Jesse van de Velde <jonesvandevelde@gmail.com>
2025-11-24 16:19:18 +0100

    push voor hulp

--- src/app/is-authenticated.guard.ts
+++ src/app/is-authenticated.guard.ts
@@ -2,23 +2,20 @@ import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import type { CanActivateFn } from '@angular/router';
 import { RoomPageService } from './room-page/room-page.service';
-import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 export const isAuthenticatedGuard: CanActivateFn = () => {
   const router = inject(Router);
   const roomPageService = inject(RoomPageService);
 
-  console.log('ik kom erin');
-
-  return of(true);
-  // return roomPageService.isAuthenticated().pipe(
-  //   tap((authenticated) => {
-  //     console.log('guard');
-
-  //     if (!authenticated) {
-  //       router.navigate(['/login']);
-  //     }
-  //   }),
-  //   map(authenticated => authenticated),
-  // );
+  return roomPageService.isAuthenticated().pipe(
+    map((isAuth) => {
+      if (isAuth) {
+        return true;
+      }
+      else {
+        return router.createUrlTree(['login']);
+      }
+    }),
+  );
 };
