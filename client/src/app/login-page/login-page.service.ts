c2a3ee483515aeb785b4efda8777a0188872d97c
Roy de Jager <roydejager@uitgeverij-deviant.nl>
2026-01-29 21:55:09 +0100

    make endpoints go through correct routes and add proxy to angular

--- client/src/app/login-page/login-page.service.ts
+++ client/src/app/login-page/login-page.service.ts
@@ -5,9 +5,7 @@ import { Injectable } from '@angular/core';
 })
 
 export class LoginPageService {
-  protected readonly apiCallLink = 'http://127.0.0.1:3000';
-
   public login(): void {
-    window.location.href = `${this.apiCallLink}/api/login`;
+    window.location.href = '/api/login';
   }
 }
